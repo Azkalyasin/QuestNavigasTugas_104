ff67d4942eadb4687006db9020c92f5140828283
Azkalyasin <yasinadzkiaazkal@gmail.com>
2025-11-07 16:51:56 +0700

    Pembuatan function awal pada file home

--- app/src/main/java/com/example/tuagaspraktikum6/view/Home.kt
+++ app/src/main/java/com/example/tuagaspraktikum6/view/Home.kt
@@ -1,2 +1,23 @@
 package com.example.tuagaspraktikum6.view
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun Awal(modifier: Modifier = Modifier) {
+
+}
